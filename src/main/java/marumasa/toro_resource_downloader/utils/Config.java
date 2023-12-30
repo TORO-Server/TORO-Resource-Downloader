95a7df99ad788171445efac6732e6b6f338bf7cf
malken21 <Malken_Marumasa@outlook.jp>
2023-12-30 17:52:06 +0900

    Update Config.java

--- src/main/java/marumasa/toro_resource_downloader/utils/Config.java
+++ src/main/java/marumasa/toro_resource_downloader/utils/Config.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
 import static marumasa.toro_resource_downloader.TORO_Resource_Downloader.MOD_ID;
 
